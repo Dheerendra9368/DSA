12a162c9c72207740154bd7665a77b624926040a
Dheerendra Kumar <kumardhirendra9368@gmail.com>
2026-08-26 21:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (30.48%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    static bool cmp(string a,string b){
+        return a.size()<b.size();
+    }
+    string shortestBeautifulSubstring(string s, int k) {
+        vector<string> arr;
+        int ans=INT_MAX;
+        int n=s.size();
+        int i=0;
+        while(i<n && s[i]!='1') i++;
+        if(i==n) return "";
+        while(i<n){
+            int j=i;
+            int cnt=0;
+            while(cnt<k && j<n){
+                if(s[j]=='1') cnt++;
+                j++;
+            }
+            if(cnt<k && j==n) break;
+            // ans=min(ans,j-i);
+            if(ans>=j-i){
+                ans=j-i;
+                string temp="";
+                for(int k=i;k<j;k++) temp+=s[k];
+                arr.push_back(temp);
+            }
+            i++;
+            while(i<n && s[i]!='1') i++;    
+            }
+            cout<<ans;
+            if(arr.size()==0) return "";
+            string t="";
+            for(string x:arr){
+                if(x.size()==ans){
+                    if(t=="") t=x;
+                    else t=min(t,x);
+                }
+            }
+            return t;
+    }
+};

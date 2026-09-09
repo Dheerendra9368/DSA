2f80d385efa457946934388d230653d5f55c614a
Dheerendra Kumar <kumardhirendra9368@gmail.com>
2026-09-09 12:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long first=1e6;
-        long long second=1e9;
-        long long third=1e12;
-        long long fourth=1e15;
-        if(n<first) return max(n-1000+1,(long long)0);
-        else if(n>=first && n<second) return first-1-1000+1+(n-first+1)*2;
+        long long first=1e6;//less than first has comma 1
+        long long second=1e9;//less than second has comma 2(max)
+        long long third=1e12;//less than second has comma 3(max)
+        long long fourth=1e15;//less than second has comma 4(max)
+        // nos from a to b =b-a+1
+        if(n<first) return max(n-1000+1,(long long)0);// nos from 1000 to n
+        else if(n>=first && n<second) return first-1-1000+1+(n-first+1)*2;//nos from 1000 to first-1 has comma 1 then nos from first to n
         else if(n>=second && n<third) return first-1-1000+1+(second-1-first+1)*2+(n-second+1)*3;
         else if(n>=third && n<fourth) return first-1-1000+1+(second-1-first+1)*2+(third-1-second+1)*3+(n-third+1)*4;
         else return first-1-999+(second-1-first+1)*2+(third-1-second+1)*3+(fourth-1-third+1)*4+(n-fourth+1)*5;

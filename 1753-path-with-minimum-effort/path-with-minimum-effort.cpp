7e81c4c4c4aff5dbcd1c6cec959970a8869b121d
Dheerendra Kumar <kumardhirendra9368@gmail.com>
2026-09-10 12:51:01 +0530

    Time: 124 ms (19.22%) | Memory: 43.9 MB (16.87%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,31 +1,33 @@
 class Solution {
-    typedef pair<int,pair<int,int>> pr;
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
-        vector<vector<int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
+    typedef pair<int,pair<int,int>> p;
+    vector<vector<int>> dir={{1,0},{0,1},{0,-1},{-1,0}};
+
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        if(n==1 && m==1) return 0;
         vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
         dis[0][0]=0;
-        priority_queue<pr,vector<pr>,greater<pr>> pq;
-        pq.push({0,{0,0}});
-        while(pq.size()>0){
-            int i1=pq.top().second.first;
-            int j1=pq.top().second.second;
-            int w=pq.top().first;
-            pq.pop();
-            if(w>dis[i1][j1]) continue;
+        queue<p> q;
+        q.push({0,{0,0}});
+        while(q.size()>0){
+            int r=q.front().second.first;
+            int c=q.front().second.second;
+            int wt=q.front().first;
+            q.pop();
+            if(dis[r][c]<wt) continue;
             for(int i=0;i<4;i++){
-                int i2=i1+dir[i][0];
-                int j2=j1+dir[i][1];
-                if(i2<0 || j2<0 || i2>=n || j2>=m) continue;
-                int wt=abs(heights[i1][j1]-heights[i2][j2]);
-                if(dis[i2][j2]>max(dis[i1][j1],wt)){
-                    dis[i2][j2]=max(dis[i1][j1],wt);
-                    pq.push({dis[i2][j2],{i2,j2}});
+                int newr=r+dir[i][0];
+                int newc=c+dir[i][1];
+                if(newr<0 || newc<0 || newr==n || newc==m) continue;
+                if(dis[newr][newc]>max(dis[r][c],abs(grid[newr][newc]-grid[r][c]))){
+                    dis[newr][newc]=max(dis[r][c],abs(grid[newr][newc]-grid[r][c]));
+                    q.push({dis[newr][newc],{newr,newc}});
                 }
             }
         }
+    
         return dis[n-1][m-1];
     }
 };

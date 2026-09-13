94e587857d6e95f235cb33597a9c203f3a3fe1da
Dheerendra Kumar <kumardhirendra9368@gmail.com>
2026-09-14 02:23:44 +0530

    Time: 351 ms (5.59%) | Memory: 81.6 MB (5.59%) - LeetSync

---
+++ 864-image-overlap/image-overlap.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        vector<vector<int>> img1cor;
+        vector<vector<int>> img2cor;
+        int n=img1.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(img1[i][j]==1) img1cor.push_back({i,j});
+                if(img2[i][j]==1) img2cor.push_back({i,j});
+            }
+        }
+        map<pair<int,int>, int> mp;
+        int ans=0;
+        for(auto ele:img1cor){
+            int x1=ele[0];
+            int y1=ele[1];
+            for(auto ele2:img2cor){
+                int x2=ele2[0];
+                int y2=ele2[1];
+                int dx=x2-x1;
+                int dy=y2-y1;
+                mp[{dx,dy}]++;
+            }
+        }
+        for(auto ele:mp) ans=max(ans,ele.second);
+        return ans;
+    }
+};
